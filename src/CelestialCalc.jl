de90da02829988f9f01c75051734ee50bdc032a4
Pedro Martins <pedrosbarbosamartins@gmail.com>
2023-12-14 00:16:19 -0300

    Add decimal to DMS/HMS functions

--- src/CelestialCalc.jl
+++ src/CelestialCalc.jl
@@ -1,23 +1,26 @@
 module CelestialCalc
 
 export DMS, HMS
-export dms_to_decimal, hms_to_decimal
+export dms_to_decimal, hms_to_decimal, decimal_to_dms, decimal_to_hms
 
 struct DMS
   degrees::Integer
   minutes::Integer
-  seconds::Integer
+  seconds::Float64
+  isnegative::Bool
 end
+
+DMS(x,y,z) = DMS(x,y,z,false)
 
 struct HMS
   hours::Integer
   minutes::Integer
-  seconds::Integer
+  seconds::Float64
 end
 
 function dms_to_decimal(dms::DMS)
-  (; degrees, minutes, seconds) = dms
-  sign = degrees < 0 ? -1 : 1
+  (; degrees, minutes, seconds, isnegative) = dms
+  sign = isnegative ? -1 : 1
   decimal_minutes = seconds / 60
   total_minutes = minutes + decimal_minutes
   decimal_degrees = total_minutes / 60
@@ -28,6 +31,25 @@ end
 function hms_to_decimal(hms::HMS)
   (; hours, minutes, seconds) = hms
   return dms_to_decimal(DMS(hours, minutes, seconds))
+end
+
+function decimal_to_dms(decimal::Float64)
+  # todo: handle cases where minutes is ~59.999 and rounds to 60
+  isnegative = decimal < 0 ? true : false
+  abs_decimal = abs(decimal)
+  degrees = trunc(Int, abs_decimal)
+  frac_abs_decimal, _ = modf(abs_decimal)
+  decimal_minutes = 60 * frac_abs_decimal
+  minutes = trunc(Int, decimal_minutes)
+  decimal_minutes_frac, _ = modf(decimal_minutes)
+  decimal_seconds = 60 * decimal_minutes_frac
+  seconds = round(decimal_seconds; digits=2)
+  return DMS(degrees, minutes, seconds, isnegative)
+end
+
+function decimal_to_hms(decimal::Float64)
+  (; degrees, minutes, seconds) = decimal_to_dms(decimal)
+  return HMS(degrees, minutes, seconds)
 end
 
 end # module CelestialCalc
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,30 @@ using CelestialCalc
 
 @testset "Angle and Time conversions" begin
 
-  @testset "DMS/HMS to decimal degree" begin
-    @test dms_to_decimal(DMS(24, 13, 18)) ≈ 24.221667
-    @test hms_to_decimal(HMS(24, 13, 18)) ≈ 24.221667
+  @testset "DMS/HMS to decimal" begin
+    @test dms_to_decimal(DMS(24,13,18)) ≈ 24.221667
+    @test hms_to_decimal(HMS(24,13,18)) ≈ 24.221667
+
+    @test dms_to_decimal(DMS(10,25,11)) ≈ 10.4197223
+    @test hms_to_decimal(HMS(10,25,11)) ≈ 10.4197223
+
+    @test dms_to_decimal(DMS(13,4,10)) ≈ 13.0694445
+    @test hms_to_decimal(HMS(13,4,10)) ≈ 13.0694445
+
+    @test dms_to_decimal(DMS(300,20,0)) ≈ 300.333333
+  end
+
+  @testset "decimal to DMS/HMS" begin
+    @test decimal_to_dms(24.221667) == DMS(24,13,18)
+    @test decimal_to_hms(24.221667) == HMS(24,13,18)
+
+    @test decimal_to_dms(20.352) == DMS(20,21,7.2)
+    @test decimal_to_hms(20.352) == HMS(20,21,7.2)
+
+    @test decimal_to_dms(10.2958) == DMS(10,17,44.88)
+    @test decimal_to_hms(10.2958) == HMS(10,17,44.88)
+
+    @test decimal_to_dms(-0.508333) == DMS(0,30,30,true)
   end
 
 end
