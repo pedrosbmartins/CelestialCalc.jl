5e6a5737818af1a91a82d2023f7ca32c4da648aa
Pedro Martins <pedrosbarbosamartins@gmail.com>
2023-12-20 01:15:22 -0300

    Refactor time tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,19 +23,17 @@ using CelestialCalc
   end
 end
 
-@testset "Time conversions to/from decimal" begin
+@testset "Time conversions" begin
   @testset "Time to decimal" begin
     @test time_to_decimal(Time(10,25,11)) ≈ 10.4197223
     @test time_to_decimal(Time(13,4,10)) ≈ 13.0694445
   end
   
-  @testset "decimal to Time" begin
+  @testset "Decimal to Time" begin
     @test decimal_to_time(20.352) == Time(20,21,7,20)
     @test decimal_to_time(10.2958) == Time(10,17,44,88)
   end
-end
 
-@testset "Time conversions" begin
   @testset "UT to GST" begin
     @test ut_to_gst(ZonedDateTime(2010,2,7,23,30,tz"UTC")) ≈ 8.698090630099976
     @test ut_to_gst(ZonedDateTime(2014,12,13,1,tz"UTC")) ≈ 6.442866622675775
