b8d61a87cb3afd1e1dd7fd15be643df29685edf3
Pedro Martins <pedrosbarbosamartins@gmail.com>
2024-01-03 06:37:49 -0300

    Implement star map plotting

--- src/CelestialCalc.jl
+++ src/CelestialCalc.jl
@@ -1,8 +1,7 @@
 module CelestialCalc
 
-using Dates
-using Printf
-using TimeZones
+using Dates, Printf
+using Plots, TimeZones
 
 export Angle
 export angle_to_decimal, time_to_decimal, decimal_to_angle, decimal_to_time
@@ -10,6 +9,7 @@ export local_to_universal_time, solar_to_prime_sidereal_time, prime_to_local_sid
 export LatLng, EquatorialCoordinates, HorizonCoordinates
 export equatorial_to_horizon
 export cartesian_projection, stereographic_projection
+export Star, MagnitudeEffect, plot_starmap
 
 struct Angle
   degrees::Integer
@@ -185,6 +185,73 @@ function stereographic_projection(hcoords::HorizonCoordinates)
   x = (1 - x) - 1
 
   return [x,y]
+end
+
+# Plotting
+
+struct Star{C<:Union{EquatorialCoordinates,HorizonCoordinates}}
+  coordinates::C
+  magnitude::Float64
+end
+
+struct MagnitudeEffect
+  min::Float64
+  factor::Float64
+  exp::Float64
+end
+
+function magnitude_effect(value::Float64, effect::MagnitudeEffect; minvalue=2, maxvalue=8)
+  (; min, factor, exp) = effect
+  min + factor * (1 - (value + minvalue)/(maxvalue + minvalue))^exp
+end
+
+function plot_starmap(stars::Array{Star{EquatorialCoordinates}},
+                      local_civilian_date::ZonedDateTime, 
+                      latlong::LatLng;
+                      projection=stereographic_projection,
+                      sizeeffect=MagnitudeEffect(0.5,3,4),
+                      alphaeffect=MagnitudeEffect(0,5,4),
+                      kwargs...)
+  stars = [Star(equatorial_to_horizon(star.coordinates, local_civilian_date, latlong), star.magnitude) for star in stars]
+  filter!(s -> s.coordinates.h >= 0, stars)
+  plot_starmap(
+    stars,
+    local_civilian_date,
+    latlong;
+    projection=projection,
+    sizeeffect=sizeeffect,
+    alphaeffect=alphaeffect,
+    kwargs...)
+end
+
+function plot_starmap(stars::Array{Star{HorizonCoordinates}},
+                      local_civilian_date::ZonedDateTime,
+                      latlong::LatLng;
+                      projection=stereographic_projection,
+                      sizeeffect=MagnitudeEffect(0.5,3,4),
+                      alphaeffect=MagnitudeEffect(0,5,4),
+                      kwargs...)
+  Projection = Matrix{Float64}(undef, length(stars), 2)
+
+  [Projection[i,:] = projection(star.coordinates) for (i,star) in enumerate(stars)]
+
+  scatter(
+    Projection[:,1],
+    Projection[:,2];
+    color="#fff",
+    background_color="#222",
+    aspect_ratio=:equal,
+    alpha=[magnitude_effect(star.magnitude, alphaeffect) for star in stars],
+    markersize=[magnitude_effect(star.magnitude, sizeeffect) for star in stars],
+    grid=false,
+    showaxis=false,
+    legend=false,
+    kwargs...
+  )
+
+  # map outline
+  tvec = range(0, 4π, length = 500)
+  plot!(sin.(tvec), cos.(tvec), color="#191919")
 end
 
 end # module CelestialCalc
